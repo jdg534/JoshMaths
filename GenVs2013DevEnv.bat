50227514db6103865015e0b552a8db3548ed24b3
Josh <joshdgibson21@outlook.com>
2017-11-06 23:38:37 +0000

    A dev environment can be build with gtest recognised as a part of the solution. next get it to compile correctly with MSVC

--- GenVs2013DevEnv.bat
+++ GenVs2013DevEnv.bat
@@ -3,8 +3,7 @@ if exist DEV_ENV rmdir /S /Q DEV_ENV
 mkdir DEV_ENV
 
 cd DEV_ENV
-cmake ../ -G "Visual Studio 12 2013"
-cmake --build .
-ctest -VV
+cmake ../ -DEnableUnitTesting=ON -G "Visual Studio 12 2013"
+
 
 PAUSE
---
+++ UnitTests/main.cpp
@@ -0,0 +1,18 @@
+#include <Math.h>
+
+#include <iostream>
+#include "gtest/gtest.h"
+
+
+
+TEST(FailTest, test)
+{
+    EXPECT_TRUE(false);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc,argv);
+    RUN_ALL_TESTS();
+    return 0;
+}
