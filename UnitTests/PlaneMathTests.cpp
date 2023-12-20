51cea453fae4d811535fddca64ba772b0582abe6
Josh <joshdgibson21@outlook.com>
2023-12-20 00:57:47 +0000

    Added unit tests for all plane math removed the check for an asset test as it prevented subsequent tests from being run.

--- UnitTests/PlaneMathTests.cpp
+++ UnitTests/PlaneMathTests.cpp
@@ -12,7 +12,51 @@ GTEST_TEST(PlaneMathTests, CreateValidPlaneWorks)
 	EXPECT_FLOAT_EQ(Plane.distanceFromOrigin, -10.0f);
 }
 
-GTEST_TEST(PlaneMathTests, CreatePlaneWithBadNormalFails)
+GTEST_TEST(PlaneMathTests, PlaneDotProductAsVectorWorks)
 {
-	EXPECT_ANY_THROW(Math::Planes::createPlane({0.0f, 10.0f, 5.0f}, -10.0f));
+	const Vector3D AVector{ 10.0f, -1.0f, 0.0f};
+	const Plane APlane = Math::Planes::createPlane({0.0f, 1.0f, 0.0f}, 0.0f);
+	const float Expected = (0.0f * 10.0f) + (1.0f * -1.0f) + (0.0f * 0.0f) + (0.0f * 0.0f);
+	const float Actual = Math::Planes::dotProductAsVector(APlane, AVector);
+	EXPECT_FLOAT_EQ(Expected, Actual);
+}
+
+GTEST_TEST(PlaneMathTests, PlaneDotProductAsPositionWorks)
+{
+	const Vector3D APosition{ 10.0f, -1.0f, 0.0f };
+	const Plane APlane = Math::Planes::createPlane({ 0.0f, 1.0f, 0.0f }, 0.0f);
+	const float Expected = (0.0f * 10.0f) + (1.0f * -1.0f) + (0.0f * 0.0f) + (0.0f * 1.0f);
+	const float Actual = Math::Planes::dotProductAsPosition(APlane, APosition);
+	EXPECT_FLOAT_EQ(Expected, Actual);
+}
+
+GTEST_TEST(PlaneMathTests, DistanceFromPlaneWorks)
+{
+	const Vector3D PositionToTest{ 0.0f, 10.0f, 0.0f };
+	const Plane TestPlane = Math::Planes::createPlane({ 0.0f, 1.0f, 0.0f }, 0.0f);
+	const float Expected = 10.0f;
+	const float Actual = Math::Planes::distanceFromPlane(TestPlane, PositionToTest);
+	EXPECT_FLOAT_EQ(Expected, Actual);
+}
+
+GTEST_TEST(PlaneMathTests, ReflectVectorWorks)
+{
+	const Vector3D VectorToTest{ 5.0f, -10.0f, 0.0f };
+	const Plane TestPlane = Math::Planes::createPlane({ 0.0f, 1.0f, 0.0f }, 0.0f);
+	const Vector3D Expected{ 5.0f, 10.0f, 0.0f };
+	const Vector3D Actual = Math::Planes::reflectVector(VectorToTest, TestPlane);
+	EXPECT_FLOAT_EQ(Expected.x, Actual.x);
+	EXPECT_FLOAT_EQ(Expected.y, Actual.y);
+	EXPECT_FLOAT_EQ(Expected.z, Actual.z);
+}
+
+GTEST_TEST(PlaneMathTests, ReflectPosition)
+{
+	const Vector3D VectorToTest{ 5.0f, -10.0f, 0.0f };
+	const Plane TestPlane = Math::Planes::createPlane({ 0.0f, 1.0f, 0.0f }, 0.0f);
+	const Vector3D Expected{ 5.0f, 10.0f, 0.0f };
+	const Vector3D Actual = Math::Planes::reflectPosition(VectorToTest, TestPlane);
+	EXPECT_FLOAT_EQ(Expected.x, Actual.x);
+	EXPECT_FLOAT_EQ(Expected.y, Actual.y);
+	EXPECT_FLOAT_EQ(Expected.z, Actual.z);
 }
